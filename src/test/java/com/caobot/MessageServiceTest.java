5d57f04d8a19b9e6dd5a842c41ac38cd648ad913
caoyu-dev <choyumin01@gmail.com>
2022-07-06 00:36:52 +0900

    [Fix] Test 수정 및 자바 버전 변경 그리고 application.yml 이 들어가는지 확인 차원에서 전부 푸쉬

--- src/test/java/com/caobot/MessageServiceTest.java
+++ src/test/java/com/caobot/MessageServiceTest.java
@@ -7,12 +7,17 @@ import org.springframework.beans.factory.annotation.Value;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MessageServiceTest {
-    @Value("${user.token}")
     private static String bot_token;
 
+    // Test 쪽은 @Value 가 안된다. 생각
+    @Value("${user.token}")
+    private void setBotToken(String value) {
+        this.bot_token = value;
+    }
+
     @Test
-    @DisplayName("token 이 제대로 들어갔는가 테스트")
+    @DisplayName("token 이 비었는가 테스트")
     public void token_test() {
-        assertThat(bot_token.equals("OTkxMDA3ODkyNDk4OTY4NjM2.GenBLb.cMcGXDTSnz-tjaA2PtPxa1F_Q9z0uG0ihS0otU"));
+        assertThat(!bot_token.isEmpty()).isTrue();
     }
 }
