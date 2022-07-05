be3f64fb5c74abab3533dc4fa29d5c03daf4ca7a
caoyu-dev <choyumin01@gmail.com>
2022-07-05 10:50:40 +0900

    [Add] 생성한 bot 에 메시지 기능 추가 1. !name 키워드를 보내면 봇의 이름인 cao-bot 반환

---
+++ src/main/java/com/caobot/CaoBotApplication.java
@@ -0,0 +1,13 @@
+package com.caobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaoBotApplication.class, args);
+	}
+
+}
