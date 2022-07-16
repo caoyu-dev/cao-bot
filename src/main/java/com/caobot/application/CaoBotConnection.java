ff3a39453530dec65411133a9bbb6374e23740fd
caoyu-dev <choyumin01@gmail.com>
2022-07-17 00:55:31 +0900

    [Fix] stop 메서드 수정 및 Activity 수정

--- src/main/java/com/caobot/application/CaoBotConnection.java
+++ src/main/java/com/caobot/application/CaoBotConnection.java
@@ -3,10 +3,12 @@ package com.caobot.application;
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.LoginException;
+import java.util.Scanner;
 
 @Service
 public class CaoBotConnection {
@@ -24,20 +26,17 @@ public class CaoBotConnection {
             jda = JDABuilder.createDefault(AccountType.BOT.name())
                     .setToken(bot_token)
                     .addEventListeners(new MessageService())
-//                    .setActivity(Activity.watching("I am Cao-Bot!"))
+                    .setActivity(Activity.watching("cao-bot 운영 중!"))
                     .build();
 
-            jda.awaitReady();
             System.out.println("Finished Building JDA!");
         } catch (LoginException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-//    public static void stop() {
-//        jda.shutdown();
-//        System.out.println("Finished shutdown JDA!");
-//    }
+    public static void stop() {
+        jda.shutdown();
+        System.out.println("Finished Stopping JDA!");
+    }
 }
